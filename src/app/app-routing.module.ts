954fb16b8ae0d5c0cc4c8850ea824a5b14280f1c
“Titaks35 <turkungr35@gmail.com>
2025-03-20 06:35:59 +0700

    fix route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,14 +5,13 @@ import { MoviesComponent } from './components/movies/movies.component';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'home', component: HomeComponent },
+  { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'movies', component: MoviesComponent },
-  { path : 'login', component: LoginComponent},
+  { path: 'login', component: LoginComponent},
   /* { path: 'movies/:title', component: MovieDetailComponent },
   { path: 'tvs', component: TvsComponent },
-  { path: 'signup', component: signupComponent },
   { path: 'profile', component: ProfileComponent }, */
-  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', redirectTo: '/'},
 ];
 
 @NgModule({
