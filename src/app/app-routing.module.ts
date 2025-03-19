d89b06eae261f1cde75517b3ebc11f2f4fbae00e
“Titaks35 <turkungr35@gmail.com>
2025-03-19 12:43:57 +0700

    move login into dir components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { MoviesComponent } from './components/movies/movies.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path : 'login', component: LoginComponent},
   { path: 'home', component: HomeComponent },
   { path: 'movies', component: MoviesComponent },
+  { path : 'login', component: LoginComponent},
   /* { path: 'movies/:title', component: MovieDetailComponent },
   { path: 'tvs', component: TvsComponent },
   { path: 'signup', component: signupComponent },
-  { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent }, */
   { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { MoviesComponent } from './components/movies/movies.component';
 import { MoviesCardComponent } from './components/movies-card/movies-card.component';
 import { MovieDetailComponent } from './components/movie-detail/movie-detail.component';
-import { LoginComponent } from './login/login.component';
+import { LoginComponent } from './components/login/login.component';
 
 @NgModule({
   declarations: [
